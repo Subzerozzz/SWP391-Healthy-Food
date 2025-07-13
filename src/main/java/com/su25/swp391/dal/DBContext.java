6ba39b3439f3645101fe3af56ebd4bb3b0dba64d
duynq112004 <nguyenduyntn112004@gmail.com>
2025-07-13 16:33:51 +0700

    Iteration 3 version 6

--- src/main/java/com/su25/swp391/dal/DBContext.java
+++ src/main/java/com/su25/swp391/dal/DBContext.java
@@ -24,7 +24,7 @@ public class DBContext {
       // Change the username password and url to connect your own database
       String username = "root";
       String password = "root";
-      String url = "jdbc:mysql://localhost:3306/Swp301_pr";
+      String url = "jdbc:mysql://localhost:3306/swp391_healthy_food";
       Class.forName("com.mysql.cj.jdbc.Driver");
       connection = DriverManager.getConnection(url, username, password);
     } catch (ClassNotFoundException | SQLException ex) {
